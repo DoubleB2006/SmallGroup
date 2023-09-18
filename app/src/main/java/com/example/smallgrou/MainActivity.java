9c0685f9fe9c387aa21ae1030097e737b9479155
DoubleB2006 <113694379+DoubleB2006@users.noreply.github.com>
2023-09-17 21:22:20 -0500

    It works
    This took a while

--- app/src/main/java/com/example/smallgrou/MainActivity.java
+++ app/src/main/java/com/example/smallgrou/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.smallgrou;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+import com.example.smallgrou.R;
 
-public class MainActivity extends AppCompatActivity {
+// @author : Phat (Phillip) H. VU <vuhongphat@hotmail.com>
+public class MainActivity extends Activity {
+
+    EditText mEtPwd;
+    CheckBox mCbShowPwd;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // get the password EditText
+        mEtPwd = (EditText) findViewById(R.id.etPassword);
+        // get the show/hide password Checkbox
+        mCbShowPwd = (CheckBox) findViewById(R.id.cbShowPwd);
+
+        // add onCheckedListener on checkbox
+        // when user clicks on this checkbox, this is the handler.
+        mCbShowPwd.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // checkbox status is changed from uncheck to checked.
+                if (!isChecked) {
+                    // hide password
+                    mEtPwd.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                } else {
+                    // show password
+                    mEtPwd.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+            }
+        });
     }
 }
